7f24f7f1901865c168153a0cf60ee38a895235b6
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-10-17 15:59:48 -0600

    Created database and added queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,23 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name = '%mon';
+
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+-- List the name of all animals that are neutered and have less than 3 escape attempts.
+SELECT name from animals WHERE neutered = true AND escape_attempt > 3;
+
+-- List the date of birth of all animals named either "Agumon" or "Pikachu".
+SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
+
+-- List name and escape attempts of animals that weigh more than 10.5kg
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+
+-- Find all animals that are neutered.
+SELECT * from animals WHERE neutered = true;
+
+-- Find all animals not named Gabumon.
+SELECT * from animals WHERE name != 'Gabumon';
+
+-- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
